1e470759168939c298becd1b31ff0e2b5ae53b79
Solar <43858034+solar-zhou@users.noreply.github.com>
2020-12-03 23:13:55 +0800

    更新代码：增加mainpanel的监听

---
+++ src/gui/listener/ConfigListener.java
@@ -0,0 +1,32 @@
+package gui.listener;
+
+import entity.Config;
+import gui.panel.ConfigPanel;
+import gui.panel.MainPanel;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ConfigListener implements DocumentListener, ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+
+    }
+}
---
+++ src/startup/Bootstrap.java
@@ -0,0 +1,21 @@
+package startup;
+
+import gui.frame.MainFrame;
+import gui.panel.MainPanel;
+import gui.panel.SpendPanel;
+import utils.GUIUtil;
+
+import javax.swing.*;
+
+public class Bootstrap {
+    public static void main(String[] args) throws Exception{
+        GUIUtil.useLNF();
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame.instance.setVisible(true);
+                MainPanel.instance.workingPanel.show(SpendPanel.instance);
+            }
+        });
+    }
+}
